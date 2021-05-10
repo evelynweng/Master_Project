4fe9aa8d62a2f75409e9290ef4db933a6f357810
Ubuntu <ubuntu@ip-172-31-8-87.us-west-1.compute.internal>
2021-05-10 02:20:05 +0000

    solve migration problem, and install text

--- uiweb/admin.py
+++ uiweb/admin.py
@@ -5,4 +5,4 @@ from .models import Store
 class StoreAdmin(admin.ModelAdmin):
     list_display = ['store_id','store_name', 'owner_first_name', 'owner_last_name', 'store_phone',
             'email', 'store_url', 'store_address','registration_date', 'store_capacity', 'password']
-admin.site.register(Store,StoreAdmin)
+#admin.site.register(Store,StoreAdmin)
